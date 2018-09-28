25b2a7e8f5eafead780e31f29a7967fa24fa3e09
wuwen1979 <wuwen1979@outlook.com>
2018-09-28 23:58:42 +0800

    study

--- lesson2.py
+++ lesson2.py
@@ -2,19 +2,15 @@
 猜数游戏
 created by 2018-09-27
 author 武文
-version:1.0
+version 1.1
 '''
 
-for i in range(1, 11):
-
-    name = int(input("COM: Guess what I think?"))
-
+while(True):
+    name = eval(input("COM: Guess what I think?"))
     if name < 10:
         print("COM: Your answer is too small.")
-        i = i + 1
     elif name > 10:
         print("COM: Your answer is too large.")
-        i = i + 1
     elif name == 10:
         print("bingo!")
         break
